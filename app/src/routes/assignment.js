466e47a6070943b53fdaf2cbde4dceef64e68bee
ijcrawford <ijcrawford@quinnipiac.edu>
2023-04-17 09:54:57 -0400

    assignment routes

--- app/src/routes/assignment.js
+++ app/src/routes/assignment.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require('mongoose');
 const router = express.Router();
 
+const Assignment = require('../models/assignment');
 const Professor = require("../models/professor");
 const Student = require("../models/student");
 const fsPromise = require('node:fs/promises');
@@ -9,41 +10,117 @@ const fs = require('fs');
 
 
 //submit assignment
-router.post("/submissions", (req, res) => {
-    
+router.post("/:assignmentId/submissions", (req, res) => {
+    try {
+        const assignmentId = req.params.assignmentId;
+        const submissions = Assignment.findById(assignmentId).submission;
+        const submission = submissions.push({
+            studentId: req.body.studentId,
+            grade: req.body.grade,
+            feedback: req.body.feedback
+        });
+
+        res.status(200).send(submission);
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 //get all submissions for an assignment
-router.get("/submissions", (req, res) => {
-    
+router.get("/:assignmentId/submissions", (req, res) => {
+    const assignmentId = req.params.assignmentId;
+    try {
+        const assignment = Assignment.findById(assignmentId);
+        assignment.submission.forEach((submission, index) => {
+            res.status(200).send(submission);
+        });
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 //get specific submission
-router.get("/submissions/:studentId", (req, res) => {
-    
+router.get("/:assignmentId/submissions/:studentId", (req, res) => {
+    const assignmentId = req.params.assignmentId;
+    const studentId = req.params.studentId;
+    try {
+        const assignment = Assignment.findById(assignmentId);
+        //const student = Student.findById(studentId);
+        const submissions = assignment.submission;
+        specSubmission = submissions.filter(submission => submission.studentId == studentId);
+
+        res.status(200).send(specSubmission);
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 //update specific submission
-router.put("/submissions/:studentId", (req, res) => {
-    
+router.put("/:assignmentId/submissions/:studentId", (req, res) => {
+    const assignmentId = req.params.assignmentId;
+    const studentId = req.params.studentId;
+    try {
+        const assignment = Assignment.findById(assignmentId);
+
+        submission.findByIdAndUpdate(studentId, 
+            {grade: grade}).then((course) => {
+                res.status(200).json(course);
+            });
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 //update feedback on submission
-router.put("/submissions/:studentId/feedback", (req, res) => {
-    
+router.put("/:assignmentId/submissions/:studentId/feedback", (req, res) => {
+    const assignmentId = req.params.assignmentId;
+    const studentId = req.params.studentId;
+    try {
+        const assignment = Assignment.findById(assignmentId);
+
+        submission.findByIdAndUpdate(studentId, 
+            {feedback: feedback}).then((course) => {
+                res.status(200).json(course);
+            });
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 //get test case output for assignment
-router.put("/testcase/:submissionId/:output", (req, res) => {
+router.put("/:assignmentId/testcase/:submissionId/:output", (req, res) => {
     
 });
 
 //get due date
-router.get("/dueDate", (req, res) => {
-    
+router.get("/:assignmentId/dueDate", (req, res) => {
+    const assignmentId = req.params.assignmentId;
+
+    try {
+        const assignment = Assignment.findById(assignmentId);
+
+        res.status(200).send(assignment.dueDate);
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
-//update due date
-router.put("/dueDate", (req, res) => {
-    
-});
+/* //update due date
+router.put("/:assignmentId/dueDate", (req, res) => {
+    const assignmentId = req.params.assignmentId;
+
+    try {
+        const assignment = Assignment.findById(assignmentId);
+
+        res.status(200).send(assignment);
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+}); */
